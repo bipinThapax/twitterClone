9158a4efd543ac0056510e1483e7125579b42a1a
bipinThapax <bipin.thapax@gmail.com>
2025-11-26 18:44:26 +0545

    designed basic layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
